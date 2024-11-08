3341ae4fdb4192e74cdce6b74220995864e68d3d
Javier Aguilar <javieraguilarropero16@gmail.com>
2024-11-08 08:56:02 +0100

    diseño HomeScreen

--- fl_components/lib/screens/alert_screen.dart
+++ fl_components/lib/screens/alert_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
 
-  const AlertScreen({super.key});
+  const AlertScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
---
+++ fl_components/lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : const Text('Compnentes de Flutter'),
+        backgroundColor: Colors.blue,
+        elevation:0,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.abc_outlined),
+          title: const Text('Nombre de ruta'),
+          onTap: () {
+
+          },
+        ),
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: 5
+      )
+      );
+  }
+}
