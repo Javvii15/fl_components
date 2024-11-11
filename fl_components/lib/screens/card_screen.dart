79e2439f84050f217bb3534866917e48a87fdc99
Javier Aguilar <javieraguilarropero16@gmail.com>
2024-11-11 11:08:44 +0100

    card screen diseño

--- fl_components/lib/screens/card_screen.dart
+++ fl_components/lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -6,10 +7,25 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
       ),
+      body: ListView(
+         children: [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.photo_album_outlined, color : AppTheme.primary),
+                  title: Text('Titulo de la tarjeta'),
+                  subtitle: Text('Ut cupidatat quis reprehenderit dolor mollit.'),
+                )
+              ],
+            )
+          )
+         ],
+      )
     );
   }
 }
