ba8ddf68f3186305731ed572f603ec94cfab1397
Javier Aguilar <javieraguilarropero16@gmail.com>
2024-11-11 12:20:48 +0100

    boton aceptar y cancelar card

--- fl_components/lib/screens/card_screen.dart
+++ fl_components/lib/screens/card_screen.dart
@@ -15,7 +15,7 @@ class CardScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         children: [
-          CustomCardTipo1()
+          CustomCardTipo1(),
          ],
       )
     );
