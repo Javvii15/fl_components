430c78b77d3e05b2b9ae963cba30db6a69ecf7f4
Javier Aguilar <javieraguilarropero16@gmail.com>
2024-11-15 09:57:24 +0100

    texto en las cinco imagenes

--- fl_components/lib/screens/card_screen.dart
+++ fl_components/lib/screens/card_screen.dart
@@ -13,18 +13,18 @@ class CardScreen extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        children: [
+        children: const [
           CustomCardTipo1(),
           SizedBox(height: 10),
-          CustomCardTipo2(imageUrl: 'https://img.rtve.es/imagenes/quien-hasbullah-magomedov/1628259969545.jpg'),
+          CustomCardTipo2(imageUrl: 'https://img.rtve.es/imagenes/quien-hasbullah-magomedov/1628259969545.jpg', nombre: 'Hasbulla fighter'),
           SizedBox(height: 10),
-          CustomCardTipo2(imageUrl: 'https://estaticos-cdn.prensaiberica.es/clip/eb71b691-aa8d-4742-a48e-dab0f9f04f6d_16-9-discover-aspect-ratio_default_0.jpg'),
+          CustomCardTipo2(imageUrl: 'https://estaticos-cdn.prensaiberica.es/clip/eb71b691-aa8d-4742-a48e-dab0f9f04f6d_16-9-discover-aspect-ratio_default_0.jpg', nombre: 'Hasbulla calvo'),
           SizedBox(height: 10),
-          CustomCardTipo2(imageUrl: 'https://www.concierto.cl/wp-content/uploads/2022/01/Hasbulla-Magomedov.jpg'),
+          CustomCardTipo2(imageUrl: 'https://www.concierto.cl/wp-content/uploads/2022/01/Hasbulla-Magomedov.jpg',),
           SizedBox(height: 10),
-          CustomCardTipo2(imageUrl: 'https://ih1.redbubble.net/image.5237492132.5051/flat,750x,075,f-pad,750x1000,f8f8f8.u2.jpg'),
+          CustomCardTipo2(imageUrl: 'https://ih1.redbubble.net/image.5237492132.5051/flat,750x,075,f-pad,750x1000,f8f8f8.u2.jpg', nombre: 'Hasbulla adinerado'),
           SizedBox(height: 10),
-          CustomCardTipo2(imageUrl: 'https://ak.uecdn.es/p/108/thumbnail/entry_id/0_3c4ynsbq/width/660/cache_st/1683708396/type/2/bgcolor/000000/0_3c4ynsbq.jpg'),
+          CustomCardTipo2(imageUrl: 'https://ak.uecdn.es/p/108/thumbnail/entry_id/0_3c4ynsbq/width/660/cache_st/1683708396/type/2/bgcolor/000000/0_3c4ynsbq.jpg', nombre:'Hasbulla trajeado'),
          ],
       )
     );
--- fl_components/lib/widgets/custom_card_tipo_2.dart
+++ fl_components/lib/widgets/custom_card_tipo_2.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class CustomCardTipo2 extends StatelessWidget {
 
   final String imageUrl;
+  final String? nombre;
 
-  const CustomCardTipo2({super.key, required this.imageUrl});
+  const CustomCardTipo2({super.key, required this.imageUrl, this.nombre});
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +24,12 @@ class CustomCardTipo2 extends StatelessWidget {
             fit: BoxFit.cover,
             fadeInDuration: const Duration(milliseconds: 3000),
           ),
+          if(nombre != null)
           Container(
             alignment: AlignmentDirectional.centerEnd,
             padding: const EdgeInsets.only(top: 10, bottom:10, right: 20),
             child: 
-            const Text('Hasbulla')
+              Text(nombre ?? 'Desconocido')
           )
         ],
       ),
