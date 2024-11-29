09dd64f3033a138e7e5b9dabc474d6a6941ffd3b
Javier Aguilar <javieraguilarropero16@gmail.com>
2024-11-29 10:00:42 +0100

    boton para enviar y widget form

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -11,41 +11,53 @@ class InputsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Forms: Inputs'),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-         child: Column(
-          children: [
-            CustomTextFormField(
-              hintText: 'Nombre',
-              labelText: 'Nombre',
-              helperText: 'Solo letras',
-              icon: Icons.verified_outlined,
-              suffixIcon: Icons.person_2_rounded,
-              obscureText: false,
-            ),
-            SizedBox(height: 30),
-            CustomTextFormField(
-              hintText: 'Apellidos',
-              labelText: 'Apellidos del usuario',
-              icon: Icons.person_4_outlined,
-              obscureText: false,
-            ),
-            SizedBox(height: 30),
-            CustomTextFormField(
-              hintText: 'E-Mail',
-              labelText: 'E-Mail del usuario',
-              icon: Icons.email_rounded,
-              keyboardType: TextInputType.emailAddress,
-              obscureText: false,
-            ),
-            SizedBox(height: 30),
-            CustomTextFormField(
-              hintText: 'Contraseña',
-              labelText: 'Contraseña del usuario',
-              icon: Icons.password_rounded,
-              obscureText: true,
-            ),
-          ]
+         child: Form(
+           child: Column(
+            children: [
+              CustomTextFormField(
+                hintText: 'Nombre',
+                labelText: 'Nombre',
+                helperText: 'Solo letras',
+                icon: Icons.verified_outlined,
+                suffixIcon: Icons.person_2_rounded,
+                obscureText: false,
+              ),
+              SizedBox(height: 30),
+              CustomTextFormField(
+                hintText: 'Apellidos',
+                labelText: 'Apellidos del usuario',
+                icon: Icons.person_4_outlined,
+                obscureText: false,
+              ),
+              SizedBox(height: 30),
+              CustomTextFormField(
+                hintText: 'E-Mail',
+                labelText: 'E-Mail del usuario',
+                icon: Icons.email_rounded,
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+              ),
+              SizedBox(height: 30),
+              CustomTextFormField(
+                hintText: 'Contraseña',
+                labelText: 'Contraseña del usuario',
+                icon: Icons.password_rounded,
+                obscureText: true,
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+           
+                }, 
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Center(child: Text('Enviar formulario'))
+                  )
+                )
+            ]
+           ),
          ),
       ),
     );
